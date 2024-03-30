2f0f51477540e8fbd62c3035c4807f2539a2758e
Eirlys Vo <voquyphiqn2004@gmail.com>
2024-03-30 14:31:44 -0400

    add toggle dark mode

--- Your K-POP Odyssey/scripts/index.js
+++ Your K-POP Odyssey/scripts/index.js
@@ -1,12 +1,7 @@
-// TODO: Query for button with an id "theme-button"
-let themeButton = document.getElementById("theme-btn");
-
-
-// TODO: Complete the toggleDarkMode function
 const toggleDarkMode = () => {
     document.body.classList.toggle("dark-mode");
 }
 
-themeButton.addEventListener("click",toggleDarkMode)
-// TODO: Register a 'click' event listener for the theme button
-// Set toggleDarkMode as the callback function.
+let themeBtn = document.getElementById("theme-btn");
+
+themeBtn.addEventListener("click", toggleDarkMode);
