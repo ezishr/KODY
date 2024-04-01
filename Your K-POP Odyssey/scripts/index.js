d57c2b834075528cfcfee7a1be45c774cd6d2a2a
Eirlys Vo <117403232+ezishr@users.noreply.github.com>
2024-04-01 18:34:45 +0000

    add & adjust unit 6 starter code html css

--- Your K-POP Odyssey/scripts/index.js
+++ Your K-POP Odyssey/scripts/index.js
@@ -5,3 +5,12 @@ const toggleDarkMode = () => {
 let themeBtn = document.getElementById("theme-btn");
 
 themeBtn.addEventListener("click", toggleDarkMode);
+
+// Add your query for the sign now button here
+let signNowButton = document.getElementById("sign-now-button");
+
+const addSignature = () => {
+    // Write your code to manipulate the DOM here
+}
+
+// Add a click event listener to the sign now button here
